8648947029caa7aeadf6df7e0818eef104715af1
Jose Gutierrez <matademango67@gmail.com>
2025-11-26 19:22:40 -0400

    Reorganize folder structure

--- backend/task.js
+++ backend/task.js
@@ -1,1 +1,25 @@
-// ...existing code...
+import express, { json } from 'express'
+import { tasks } from "./model/model.js"
+import { TaskRouter } from "./view/router.js"
+import cors from "cors"
+import path from 'path'
+
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 3000
+
+
+app.use(cors());
+app.use(json()) 
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 3000")
+})
+
+app.use(TaskRouter) 
+
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "frontend")));
--- backend/view/router.js
+++ backend/view/router.js
@@ -1,1 +1,11 @@
-// ...existing code...
+import { Router } from "express";
+//import {Task_controller} from '../controler/controlador.js'
+import { db_controller } from '../controler/controlador.js';
+
+export const TaskRouter = Router()
+
+TaskRouter.get('/', db_controller.getTasks)
+TaskRouter.post('/',db_controller.create )
+TaskRouter.delete('/:title', db_controller.delete )
+TaskRouter.get('/:title', db_controller.getByTitle)
+TaskRouter.patch('/:title', db_controller.update)
